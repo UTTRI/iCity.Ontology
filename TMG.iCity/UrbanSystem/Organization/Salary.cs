99744a60e47889c6dcbc7350f993e4053c902cef
JamesVaughan <James.Vaughan@utoronto.ca>
2017-03-28 17:18:12 -0400

    Added stubs for the Organization ontology.

---
+++ TMG.iCity/UrbanSystem/Organization/Salary.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Copyright 2017 University of Toronto
+
+    This file is part of iCity Ontology.
+
+    iCity Ontology is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    iCity Ontology is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with iCity Ontology.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TMG.iCity.Foundational.Monetary;
+
+namespace TMG.iCity.UrbanSystem.Organization
+{
+    public struct Salary
+    {
+        public MonetaryValue AnnualPay { get; private set; }
+
+        public Salary(MonetaryValue annualPay)
+        {
+            AnnualPay = annualPay;
+        }
+    }
+}
